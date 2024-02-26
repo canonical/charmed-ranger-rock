4057685e9cc31c8d728598c6a373ebe45976148f
Amber Charitos <amber.charitos@canonical.com>
2024-02-26 15:00:01 +0000

    add ranger admin rock

---
+++ helper-scripts/usersync-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+
+cd /usr/lib/ranger/usersync && \
+./setup.sh && \
+./ranger-usersync-services.sh start  && \
+ls -la && \
+tail -f /var/log/ranger/usersync/usersync-ranger-k8s-*.log
