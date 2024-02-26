31d132faedbac4118025183392fa77b01de93137
Amber Charitos <amber.charitos@canonical.com>
2024-02-26 08:57:53 +0000

    remove main commit

--- helper-scripts/ranger-usersync-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
-
-cd /usr/lib/ranger/usersync && \
-./setup.sh && \
-ranger-usersync-services.sh start  && \
-ls -la && \
-tail -f ./ranger-*-usersync/logs/usersync-ranger-k8s-0-.log
